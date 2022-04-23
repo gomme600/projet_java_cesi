10c04d045e18608d112438c03130b6345071cc43
gomme600 <gomme600@hotmail.com>
2022-04-23 15:56:01 +0200

    Started adding logic for products and orders

--- example_project/src/Vue/Responsable.java
+++ example_project/src/Vue/Responsable.java
@@ -10,8 +10,19 @@ import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 import javax.swing.JList;
 import javax.swing.JSplitPane;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Responsable extends JFrame {
 
@@ -51,7 +62,7 @@ public class Responsable extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Portail Responsable");
-		lblNewLabel_1.setBounds(323, 23, 115, 14);
+		lblNewLabel_1.setBounds(301, 23, 137, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
@@ -59,16 +70,68 @@ public class Responsable extends JFrame {
 		contentPane.add(tabbedPane);
 		
 		JPanel panel = new JPanel();
+
 		tabbedPane.addTab("Produits", null, panel, null);
+
+		JList listproduits = new JList();
+		panel.add(listproduits);
 		
-		JList list = new JList();
-		panel.add(list);
+		JButton btnNewButton = new JButton("Rafraichir");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button pressed !");
+				try {
+				FileInputStream fstream = new FileInputStream("./database.db");
+			    DataInputStream in = new DataInputStream(fstream);
+			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				DefaultListModel listModel = new DefaultListModel();
+				String strLine;
+				while ((strLine = br.readLine()) != null)   
+				{
+				        listModel.addElement(strLine); 
+				        System.out.println(strLine);
+				}
+
+				listproduits.setModel(listModel);
+				in.close();
+				}
+			    catch (Exception ee) {
+			    }
+			}
+			
+		});
+		panel.add(btnNewButton);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Commandes", null, panel_1, null);
 		
 		JList list_1 = new JList();
 		panel_1.add(list_1);
+		
+		JButton btnNewButton_1 = new JButton("Rafraichir");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button pressed !");
+				try {
+				FileInputStream fstream = new FileInputStream("./database.db");
+			    DataInputStream in = new DataInputStream(fstream);
+			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				DefaultListModel listModel = new DefaultListModel();
+				String strLine;
+				while ((strLine = br.readLine()) != null)   
+				{
+				        listModel.addElement(strLine); 
+				        System.out.println(strLine);
+				}
+
+				list_1.setModel(listModel);
+				in.close();
+				}
+			    catch (Exception ee) {
+			    }
+			}
+		});
+		panel_1.add(btnNewButton_1);
 	}
 
 	public JLabel getLblNewLabel() {
