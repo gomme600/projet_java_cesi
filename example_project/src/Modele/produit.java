e63bb7082cdb5a5a6dce2416914cb4241f0452cc
RyukSylux <momo.derien@gmail.com>
2022-04-25 11:27:04 +0200

    gestionaire de stok fonctionne

--- example_project/src/Modele/produit.java
+++ example_project/src/Modele/produit.java
@@ -18,4 +18,25 @@ public class produit {
 		} 
 	}
 	
+	public static void ajouterProduit(String nom, String prix, String quantite) {
+		GestionBDD g = new GestionBDD();
+		
+		try {
+			g.ajouterProduit(nom, prix, quantite);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	public static void supprimerProduit(String nom) {
+		GestionBDD g = new GestionBDD();
+		
+		try {
+			g.supprimerProduit(nom);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
 }
